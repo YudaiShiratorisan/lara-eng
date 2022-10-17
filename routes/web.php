21a40f09dcffee7cdffd527c449a62be7ee7eb7a
YudaiShiratorisan <112676868+YudaiShiratorisan@users.noreply.github.com>
2022-10-17 13:06:59 +0900

    create-file編集

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/create-file', function () {
-    return view('/create-file');
+    return view('create-file');
 });
 
 // Route::get('/create-file', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
