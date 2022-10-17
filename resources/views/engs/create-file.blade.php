2720f1e589845649d836465eb3fd851f316a45c3
YudaiShiratorisan <112676868+YudaiShiratorisan@users.noreply.github.com>
2022-10-17 12:53:28 +0900

    create-file 編集しました

---
+++ resources/views/engs/create-file.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <button type="button" onclick="history.back()">戻る</button>
+  <title>ファイルを追加</title>
+</head>
+<body>
+  <div style="padding: 20px; margin-bottom: 20px; border: 5px double #333333;">
+    <div style="padding: 10px; margin-bottom: 10px; border: 1px solid #333333;">
+      ファイル名
+    </div>
+  </div>
+  <button type="submit">登録完了</button>
+</body>
+</html>
