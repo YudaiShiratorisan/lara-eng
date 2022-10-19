07f6a1d92f1fbcc37ea499522ef2dd834695a33a
YudaiShiratorisan <112676868+YudaiShiratorisan@users.noreply.github.com>
2022-10-19 12:03:01 +0900

    create-file 編集しました

--- resources/views/engs/create-file.blade.php
+++ resources/views/engs/create-file.blade.php
@@ -4,60 +4,27 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset('css/create-file.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/create-file.css') }}">
+</head>
+
+<body>
   <header>
     <div class="header-left">
       <button type="button" onclick="history.back()">戻る</button>
     </div>
     <div class="header-center">
-      <h2>ファイルを編集</h2>
+      <h2>ファイルを追加</h2>
     </div>
-
-
-    <style>
-      header {
-        background-color: rgb(243, 197, 197);
-      }
-
-      .header-left{
-        justify-content: center;
-        margin: 20px;
-      }
-
-      .header-center{
-        display: flex;
-        justify-content: center;
-      }
-
-    </style>
-
   </header>
-
-</head>
-
-
-<body>
-  <div style="padding: 40px; margin-bottom: 40px; border: 5px double white;">
+  <div style="padding: 55px; margin: 50px 40px 40px 40px; border: 5px double white;">
       <input class="file-name" type="text" placeholder="ファイル名">
   </div>
   <button class="btn1" type="submit">登録完了</button>
+
+
+  {{-- <button> 登録完了 </button> --}}
+
+
 </body>
 
-<style>
-  body {
-    background-color: black;
-  }
-
-  .file-name{
-    background-color: azure;
-    padding: 15px; 
-    
-  }
-  .btn1{
-    display: flex;
-    justify-content: center;
-    align-content: center;
-    margin-left: 600px;
-  }
-</style>
 </html>
